8ed791aa4398130d7e39547719a285183fbcd441
Nguynnie1410 <tranthihuongnguyen2006@gmail.com>
2025-12-06 14:20:45 +0700

    Add function to view contacts in phonebook

--- contacts.py
+++ contacts.py
@@ -11,3 +11,14 @@ def add_contact():
     print("Đã thêm liên hệ.")
 
 
+def view_contacts():
+    if not phonebook:
+        print("Danh bạ rỗng.")
+        return
+    
+    print("\n--- DANH SÁCH LIÊN HỆ ---")
+    for i, contact in enumerate(phonebook, start=1):
+        print(f"{i}. {contact['name']} - {contact['phone']}")
+
+
+
